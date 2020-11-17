8e22395662d56e51d2e0567c404925153ae78416
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-11-17 22:26:15 +0400

    JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+const count = 23;
+let cats = 15;
+const dogs = 10;
+const name = true;
+
 function App() {
   return (
     <div className="App">
@@ -9,8 +14,28 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style={{
+        	color: '#ADD8E6',
+        	fontSize: 36 + 45
+
+        }}>
           Hello World!
+        </p>
+        <p>
+          {count}
+        </p>
+        <p>
+          {`number of animals is  ${cats + dogs}`}
+        </p>
+        <p>
+          {name && 'name is true'}
+        </p>
+        <p>
+          {(cats > 10) ? 'there are more cats than dogs' : 'there are more dogs than cats'}
+          {undefined}
+        	{null}
+	        {false}
+	        {true}
         </p>
         <a
           className="App-link"
