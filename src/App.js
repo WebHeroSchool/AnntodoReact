a56c3cbdca1f474bcf77899082b2f4d105259a3b
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-11-20 21:11:12 +0400

    React-element

--- src/App.js
+++ src/App.js
@@ -1,57 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, length} from './number';
+import React from 'react';
 
-const wrap = 23;
-let cats = 15;
-const dogs = 10;
-const name = true;
+const element = (<div>
+	Hello World!
+	<a href="">go to</a>
+</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{
-        	color: '#ADD8E6',
-        	fontSize: 36 + 45
-
-        }}>
-          Hello World!
-        </p>
-        <p>
-          {wrap}
-        </p>
-        <p>
-          {`number of animals is  ${cats + dogs}`}
-        </p>
-        <p>
-          {name && 'name is true'}
-        </p>
-        <p>
-          {(cats > 10) ? 'there are more cats than dogs' : 'there are more dogs than cats'}
-          {undefined}
-        	{null}
-	        {false}
-	        {true}
-        </p>
-        <p>
-        	{`count * length =  ${count * length}`}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element};
--- src/number.js
+++
@@ -1,4 +0,0 @@
-const count = 15;
-const length = 26;
-
-export {count, length};
