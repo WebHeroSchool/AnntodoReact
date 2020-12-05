1a9b4d44087f7faad763316cb108d0f172af5360
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-12-05 22:47:40 +0400

    Стилизация приложения - исправлено

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,19 +3,30 @@ import styles from './Footer.module.css';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+const style = {
+	opacity: '0.5',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #f50057 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
+
 const Footer = ({ count}) => (<div>
 	<div className={styles.counter}>
 		Осталось выполнить вот столько дел: { count }
 	</div>
 	<div className={styles.filter}>
 		<ButtonGroup color="secondary" aria-label="outlined secondary button group">
-			<Button>Все</Button>
-			<Button>Активные</Button>
-			<Button>Выполненные</Button>
+			<Button style={style}>Все</Button>
+			<Button style={style}>Активные</Button>
+			<Button style={style}>Выполненные</Button>
 		</ButtonGroup>
 	</div>
-	<div>
-		<Button variant="outlined" color="secondary">
+	<div className={styles.delete}>
+		<Button style={style} variant="outlined" color="secondary">
   			Удалить выполненные дела
 		</Button>
 	</div>
