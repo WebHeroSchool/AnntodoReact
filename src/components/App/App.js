238dc234fce4d3d990ca1640d2557aa7909b9312
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-12-05 21:44:54 +0400

    Стилизация приложения

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,6 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const todoItem = 'Создать новое приложение';
-
 const App = () => {
 	const items = [
 		{
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-const Footer = ({ count}) => (<div>Вот сколько дел осталось сделать: { count }</div>);
+const Footer = ({ count}) => (<div>
+	<div className={styles.counter}>
+		Осталось выполнить вот столько дел: { count }
+	</div>
+	<div className={styles.filter}>
+		<ButtonGroup color="secondary" aria-label="outlined secondary button group">
+			<Button>Все</Button>
+			<Button>Активные</Button>
+			<Button>Выполненные</Button>
+		</ButtonGroup>
+	</div>
+	<div>
+		<Button variant="outlined" color="secondary">
+  			Удалить выполненные дела
+		</Button>
+	</div>
+</div>);
 
 export default Footer;
