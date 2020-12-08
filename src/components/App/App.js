7d83f8f22a81f307891dcaad3ebe868e99ece9f9
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2020-12-08 21:23:56 +0400

    Обработка событий

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,15 +23,17 @@ class App extends React.Component {
 
 	};
 	
+	onClickDone = isDone => console.log(isDone);
+
 	render() {
 		return (
 			<div className={styles.wrap}>
 				<h1 className={styles.title}>Важные дела:</h1>
 				<InputItem />
-				<ItemList items={this.state.items} />
+				<ItemList items={this.state.items} onClickDone={this.onClickDone} />
 				<Footer count={3} />
 			</div>);
-			}
+	}
 }
 
 export default App;
